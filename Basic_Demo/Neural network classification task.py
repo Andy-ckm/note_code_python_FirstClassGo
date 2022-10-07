196fe5374302949e34f894e88804244be901f27e
Andy_Chen <65456816+Andy-ckm@users.noreply.github.com>
2022-10-07 15:54:56 +0800

    data-update-minist_1

--- Basic_Demo/Neural network classification task.py
+++ Basic_Demo/Neural network classification task.py
@@ -1,7 +1,39 @@
 from pathlib import Path
 import requests
+import pickle
+import gzip
+from matplotlib import pyplot
+import numpy as np
 
 DATA_PATH = Path("data")
 PATH = DATA_PATH / "mnist"
 
-PATH.mkdir(parents=True, )
+PATH.mkdir(parents=True, exist_ok=True)
+
+URL = "http://deeplearning.net/data/mnist/"
+FILENAME = "data_minist/mnist/minist.pkl.gz"
+
+# 判断是否存在本地文件，否，DOWNLOAD
+if not (PATH / FILENAME).exists():
+    content = requests.get(URL + FILENAME).content
+    (PATH / FILENAME).open("wb").write(content)
+with gzip.open((PATH / FILENAME).as_posix(), "rb") as f:
+    '''
+    protocol>=1,文件需要是二进制才能打开;MNIST数据包为PKL文件格式.
+
+ 
+    #创建一个字典变量
+    data = {'a':[1,2,3],'b':('string','abc'),'c':'hello'}
+    print(data)
+ 
+    #以二进制方式来存储,rb,wb,wrb,ab(新建，二进制，写入)
+    pic = open(r'.\testdata.pkl', 'wb')
+ 
+    #将字典数据存储为一个pkl文件
+    pickle.dump(data,pic)
+    pic.close()
+    '''
+    ((x_train, y_train), (x_valid, y_valid), _) = pickle.load(f, encoding='latin-1')
+
+pyplot.imshow(x_train[0].reshape((28, 28)), cmap="gray")
+print(x_train.shape)
