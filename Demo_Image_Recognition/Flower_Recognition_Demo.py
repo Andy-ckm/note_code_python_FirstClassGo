1a3692d96bf189ad784ec82d64fc536595946eba
Andy_Chen <65456816+Andy-ckm@users.noreply.github.com>
2022-10-10 14:48:27 +0800

    Images_Recognition_Demo

--- Demo_Image_Recognition/Flower_Recognition_Demo.py
+++ Demo_Image_Recognition/Flower_Recognition_Demo.py
@@ -26,8 +26,10 @@ valid_dir = data_dir + '/valid'
 
 data_transforms = {
     'train':
+        # 按顺序对原始图片做增强
         transforms.Compose([
             # H * W
+            # 较为合理的尺寸64, 128, 256, 224
             transforms.Resize([96, 96]),
             # 随机旋转，-45到45度之间随机选
             transforms.RandomRotation(45),
@@ -38,13 +40,14 @@ data_transforms = {
             # 随机垂直翻转
             transforms.RandomVerticalFlip(p=0.5),
             # 参数1为亮度，参数2为对比度，参数3为饱和度，参数4为色相
+            # 场景为：过度曝光,窄距，光线不足...
             transforms.ColorJitter(brightness=0.2, contrast=0.1, saturation=0.1,
                                    hue=0.1),
-            # 概率转换成灰度率，3通道就是R=G=B
+            # 概率转换成灰度率，eg.RGB --> RRR or RRB ...
             transforms.RandomGrayscale(p=0.025),
             # 转为Tensor
             transforms.ToTensor(),
-            # 均值，标准差
+            # 均值，标准差(借鉴imageNet的参数)
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
     'valid':
         transforms.Compose([
@@ -59,6 +62,37 @@ image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transf
 dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True) for x in ['train',
                                                                                                                 'valid']
                }
+# 计算训练集和验证集的个数
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'valid']}
 class_names = image_datasets['train'].classes
 
+# 读取标签对应的实际名字
+
+with open('cat_to_name.json', 'r') as f:
+    cat_to_name = json.load(f)
+
+# 加载现有的模型，并直接使用其已训练的权重当初始化参数
+# 常用的特征提取器，backbone
+model_name = 'resnet'
+feature_extract = True
+
+# 使用GPU训练
+train_on_gpu = torch.cuda.is_available()
+
+if not train_on_gpu:
+    print('CUDA is not available.  Training on CPU ...')
+else:
+    print('CUDA is available!  Training on GPU ...')
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# 定义模型参数的更新与否
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+model_ft = models.resnet152()
+
+def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
+
+    model_ft = models.resnet152(pretrained=use_pretrained)
